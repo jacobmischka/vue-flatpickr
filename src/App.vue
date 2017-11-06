35bef0f7f13912e70b2ec2d9aca04eb1876a0462
Jacob Mischka <jacob@mischka.me>
2017-11-06 11:42:36 -0600

    Tabify App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="app">
-    <h1>@jacobmischka/vue-flatpickr</h1>
-    <p>{{ date || 'MM-DD-YYYY' }}</p>
-    <VueFlatpickr :options="options" v-model="date" />
-  </div>
+	<div id="app">
+		<h1>@jacobmischka/vue-flatpickr</h1>
+		<p>{{ date || 'MM-DD-YYYY' }}</p>
+		<VueFlatpickr :options="options" v-model="date" />
+	</div>
 </template>
 
 <script>
@@ -12,61 +12,61 @@ import 'flatpickr/dist/flatpickr.css';
 import 'flatpickr/dist/themes/airbnb.css';
 
 export default {
-  data () {
-    return {
-      date: null,
-      options: {
-        allowInput: true,
-        enableTime: true,
-        time_24hr: true,
-		    mode: 'multiple',
-        conjunction: '; '
-      }
-    }
-  },
-  components: {
-    VueFlatpickr
-  }
+	data () {
+		return {
+			date: null,
+			options: {
+				allowInput: true,
+				enableTime: true,
+				time_24hr: true,
+				mode: 'multiple',
+				conjunction: '; '
+			}
+		}
+	},
+	components: {
+		VueFlatpickr
+	}
 }
 </script>
 
 <style lang="less">
-  html,
-  body {
-    height: 100%;
-    overflow: hidden;
-    background: #4fc08d;
-    color: #fff;
-  }
-  #app {
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    h1,
-    h4 {
-      margin-bottom: 0;
-    }
-    h1 {
-      font-size: 48px;
-    }
-    p {
-      margin-bottom: 0;
-    }
-    input {
-      padding: 10px;
-      margin-top: 20px;
-      width: 200px;
-      outline: none;
-      background: none;
-      border: 2px solid #fff;
-      color: #fff;
-    }
-    a {
-      color: #fff;
-      margin-top: 20px;
-      outline: none;
-    }
-  }
+	html,
+	body {
+		height: 100%;
+		overflow: hidden;
+		background: #4fc08d;
+		color: #fff;
+	}
+	#app {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		h1,
+		h4 {
+			margin-bottom: 0;
+		}
+		h1 {
+			font-size: 48px;
+		}
+		p {
+			margin-bottom: 0;
+		}
+		input {
+			padding: 10px;
+			margin-top: 20px;
+			width: 200px;
+			outline: none;
+			background: none;
+			border: 2px solid #fff;
+			color: #fff;
+		}
+		a {
+			color: #fff;
+			margin-top: 20px;
+			outline: none;
+		}
+	}
 </style>
